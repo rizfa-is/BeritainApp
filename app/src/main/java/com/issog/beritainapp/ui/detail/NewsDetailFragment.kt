8aa9754b2233645ebe19310418261b68b7662cec
Rosyid Rosadi <rosyidrosadi15@gmail.com>
2025-03-09 00:46:28 +0700

    feat(webview): create news detail page using webview

---
+++ app/src/main/java/com/issog/beritainapp/ui/detail/NewsDetailFragment.kt
@@ -0,0 +1,72 @@
+package com.issog.beritainapp.ui.detail
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.issog.beritainapp.databinding.FragmentNewsDetailBinding
+import com.issog.core.utils.gone
+import com.issog.core.utils.visible
+
+class NewsDetailFragment : Fragment() {
+    private lateinit var binding: FragmentNewsDetailBinding
+    private lateinit var mWebView: WebView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNewsDetailBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        handleBackPressed()
+    }
+
+    private fun handleBackPressed() {
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (mWebView.canGoBack()) {
+                    mWebView.goBack()
+                } else {
+                    findNavController().popBackStack()
+                }
+            }
+        })
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        binding.apply {
+            ivClose.setOnClickListener { findNavController().popBackStack() }
+
+            val url = arguments?.getString("url")
+            mWebView = webview
+            mWebView.settings.javaScriptEnabled = true
+            mWebView.webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    binding.pbWebview.visible()
+                }
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                    binding.tvUrl.text = view?.url?.substringAfter("://")?.split('/')?.get(0).orEmpty()
+                    binding.pbWebview.gone()
+                }
+            }
+            mWebView.webChromeClient = WebChromeClient()
+            mWebView.loadUrl(url.orEmpty())
+        }
+    }
+}
