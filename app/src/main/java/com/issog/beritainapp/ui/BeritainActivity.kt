3c2e9001dc35b25cdd9743e3c5e8602731cc1c91
Rosyid Rosadi <rosyidrosadi15@gmail.com>
2025-06-12 11:44:18 +0700

    fix(favorite) : - fix can't navigate into detail news - fix query delete didn't work

--- app/src/main/java/com/issog/beritainapp/ui/BeritainActivity.kt
+++ app/src/main/java/com/issog/beritainapp/ui/BeritainActivity.kt
@@ -3,10 +3,13 @@ package com.issog.beritainapp.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
 import com.issog.beritainapp.R
 import com.issog.beritainapp.databinding.ActivityBeritainBinding
+import com.issog.core.domain.model.ArticleModel
 
 class BeritainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBeritainBinding
@@ -21,5 +24,22 @@ class BeritainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initStartDestination()
+    }
+
+    private fun initStartDestination() {
+        val news = intent.extras?.getParcelable<ArticleModel>("news")
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val startDestination = news?.let {
+            R.id.newsDetailFragment
+        } ?: run {
+            R.id.homeFragment
+        }
+
+        val navController = navHostFragment.navController
+        val navGraph = navController.navInflater.inflate(R.navigation.nav_graph_home)
+        navGraph.setStartDestination(startDestination)
+        navController.setGraph(navGraph, bundleOf("news" to news))
     }
 }
